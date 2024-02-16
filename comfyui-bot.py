98e07438860e4265a6926ac1aa0c453aa39eacd5
Mario López Sáez <145022989+mariokhz@users.noreply.github.com>
2024-02-16 23:32:19 +0100

    Update comfyui-bot.py

--- comfyui-bot.py
+++ comfyui-bot.py
@@ -12,7 +12,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 # !gen "prompt" in discord
 @bot.command(name='gen')
-async def _genera(ctx, arg1):
+async def _gen(ctx, arg1):
     imgresult = genimg(arg1).save("resultado.png")
     await ctx.send(file=discord.File("resultado.png"))
 bot.run(TOKEN)
